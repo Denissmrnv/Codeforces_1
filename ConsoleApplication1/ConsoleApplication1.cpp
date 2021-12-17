4be315c03e46858ef95845721adf43e6bc7a73f6
unknown <denissmirnov.rsatu@gmail.com>
2021-12-17 16:50:30 +0300

    only_one

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int w;
+	cin >> w;
+	if ((w >= 1) && (w <= 100)) {
+		if (w == 2)
+			cout << "NO\n";
+		else
+			(w % 2 == 0) ? cout << "YES\n" : cout << "NO\n";
+	}
+}
